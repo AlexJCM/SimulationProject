3798078135e4c11a20728b5116b59b4799e23125
diego <diego-stainl@hotmail.com>
2018-11-27 09:10:16 -0500

    qqq

--- src/proyectosimulacion/ProyectoSimulacion.java
+++ src/proyectosimulacion/ProyectoSimulacion.java
@@ -7,14 +7,7 @@ public class ProyectoSimulacion {
     public static void main(String[] args) {
 
         System.err.println("primer comin diego");
-        System.out.println("Pavel");
 
-     
-
-        System.out.println("werwerwe");
-
-        System.out.println("");
-        System.out.println("Pavel");
 
 
     }
