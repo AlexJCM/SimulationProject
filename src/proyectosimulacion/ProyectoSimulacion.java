1913d0511a36011077783b61e239059dafa11237
Pavel Rivadeneira <pavel.rivadeneira@gmail.com>
2018-11-27 09:08:44 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/proyectosimulacion/ProyectoSimulacion.java
+++ src/proyectosimulacion/ProyectoSimulacion.java
@@ -8,9 +8,15 @@ public class ProyectoSimulacion {
 
         System.err.println("primer comin diego");
         System.out.println("Pavel");
+<<<<<<< Updated upstream
      
 
         System.out.println("werwerwe");
+=======
+
+        System.out.println("");
+        System.out.println("Pavel");
+>>>>>>> Stashed changes
 
     }
     
