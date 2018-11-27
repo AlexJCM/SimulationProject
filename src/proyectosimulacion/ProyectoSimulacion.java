18ec8e51038cf7d71386a68ae471e7084345404b
diego <diego-stainl@hotmail.com>
2018-11-27 08:43:27 -0500

    segunda prueba
    12122

--- src/proyectosimulacion/ProyectoSimulacion.java
+++ src/proyectosimulacion/ProyectoSimulacion.java
@@ -16,7 +16,7 @@ public class ProyectoSimulacion {
      */
     public static void main(String[] args) {
         System.err.println("primer comin diego");
-        System.out.println("PAvel");
+     
     }
     
 }
