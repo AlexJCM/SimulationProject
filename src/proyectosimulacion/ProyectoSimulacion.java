00e9363552d199d6948e8dafccaeb0fb19a57af3
diego <diego-stainl@hotmail.com>
2018-11-27 09:07:52 -0500

    wqeqweqe

--- src/proyectosimulacion/ProyectoSimulacion.java
+++ src/proyectosimulacion/ProyectoSimulacion.java
@@ -1,20 +1,17 @@
 
 package proyectosimulacion;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 public class ProyectoSimulacion {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.err.println("primer comin diego");
         System.out.println("Pavel");
      
-=======
-        System.out.println("");
->>>>>>> master
+
+        System.out.println("werwerwe");
+
     }
     
 }
