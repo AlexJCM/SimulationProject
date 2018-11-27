981af6858ccd84d86bb83ca6203f4259f345e1e4
Pavel Rivadeneira <pavel.rivadeneira@gmail.com>
2018-11-27 09:04:25 -0500

    creacion de metodos
    as

---
+++ src/proyectosimulacion/metodos.java
@@ -0,0 +1,12 @@
+
+package proyectosimulacion;
+
+
+public class metodos {
+    public void cola1(){
+        int horaLlegada;
+        int nClientes=0;
+        int clientesAtendidos;
+        int arreglo[] = new int[nClientes];
+    }
+}
